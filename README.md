242286e36b85e886f7ca3865dbc1b7e2e13081f9
RafaelaMorsiani <rafa.morciani@gmail.com>
2026-04-05 13:12:23 +0200

    Envio inicial

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Atv_Formativa_1
+
+# Cotação USD → BRL
+
+Projeto simples em Python que busca a cotação do dólar.
---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+
+def buscar_cotacao():
+    url = "https://api.exchangerate-api.com/v4/latest/USD"
+
+    try:
+        response = requests.get(url)
+        data = response.json()
+
+        cotacao = data["rates"]["BRL"]
+
+        print(f"1 USD = R$ {cotacao:.2f}")
+
+    except Exception as e:
+        print("Erro:", e)
+
+if __name__ == "__main__":
+    buscar_cotacao()
