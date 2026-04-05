8ed2b609c49f60346778a558b37e7491409fce02
RafaelaMorsiani <rafa.morciani@gmail.com>
2026-04-05 13:48:25 +0200

    add header to output + improve code organization

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ def buscar_cotacao():
     url = "https://api.exchangerate-api.com/v4/latest/USD"
 
     try:
+        print("===== COTAÇÃO ATUAL =====")
+
         response = requests.get(url)
         data = response.json()
 
+        #USD to BRL
         cotacao = data["rates"]["BRL"]
-
         print(f"1 USD = R$ {cotacao:.2f}")
 
+        #USD to EUR 
         eur = data["rates"]["EUR"]
         print(f"1 USD = € {eur:.2f}")
 
